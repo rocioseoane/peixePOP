f09feb9c4c48acef51ec664af3bd4d0cab50cf4d
FlagshipDev <68760931+FlagshipDev@users.noreply.github.com>
2021-11-19 21:07:32 +0100

    Añadidas dependencias Spring Cloud

--- API-REST/src/main/java/App/Restart/RestartController.java
+++ API-REST/src/main/java/App/Restart/RestartController.java
@@ -1,15 +1,23 @@
 package App.Restart;
 
-import App.App;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class RestartController {
 
+    @Autowired
+    private RestartService restartService;
+
     @PostMapping("/restart")
     public void restart() {
-        App.restart();
+        Application.restart();
     }
+
+    @PostMapping("/restartApp")
+    public void restartUsingActuator() {
+        restartService.restartApp();
+    }
+
 }
